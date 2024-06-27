98bc03cf8cabb7cd1d51e60d1322026525dcd1c8
R2sipho <r2sipho@gmail.com>
2024-06-27 13:21:22 +0000

    task

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+if __name__ == "__main__":
+    n = 5
+    triangle = pascal_triangle(n)
+    print_triangle(triangle)
+
